81d40a6f54c8a88b853217cf0de73ecd14f7bf4c
PauL <paul.helleu@orange.fr>
2020-10-01 19:56:36 +0200

    upd

--- src/app.js
+++ src/app.js
@@ -10,3 +10,17 @@ dateElement.append(
     weekday: "long",
   })
 );
+
+const dateArr = (date) => {
+  const arr = date.split(" ");
+  firstLetterUp(arr);
+};
+
+const firstLetterUp = (arr) => {
+  const text = arr.map((elem) => {
+    return elem.charAt(0).toUpperCase() + elem.substring(1);
+  });
+};
+
+console.log(date);
+dateArr(date.toString());
