dc3dd7b2f8797930914dbc758bad6077ea4a17bd
PauL <paul.helleu@orange.fr>
2020-10-02 12:40:18 +0200

    live date

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import "./style.scss";
 
-const dateElement = document.querySelector("h1");
+const dateDayElement = document.querySelector("h4");
+const dateHoursElement = document.querySelector(".content");
+const dateYearsElement = document.querySelector("footer");
 
-const dateDayNow = new Date().toLocaleDateString("fr-FR", {
+const dateDayNow = new Date().toLocaleString("fr-FR", {
   day: "numeric",
   weekday: "long",
+});
+
+const dateYears = new Date().toLocaleString("fr-FR", {
+  year: "numeric",
 });
 
 const DayNow = dateDayNow.toString();
@@ -18,9 +24,27 @@ const firstLetterUp = (arr) => {
   const text = arr.map((elem) => {
     return elem.charAt(0).toUpperCase() + elem.substring(1);
   });
-  dateElement.append(text);
+  dateDayElement.innerHTML = "";
+  dateDayElement.append(text.join(" - "));
 };
 
-// arr to text au lieu de map()
+const setHours = () => {
+  const dateHoursNow = new Date().toLocaleString("fr-FR", {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  });
+  dateHoursElement.innerHTML = "";
+  dateHoursElement.append(dateHoursNow);
+};
 
-dateArr(DayNow.toString());
+const setYears = () => {
+  dateYearsElement.innerHTML = "";
+  dateYearsElement.append(dateYears);
+};
+
+setInterval(() => {
+  dateArr(DayNow);
+  setHours();
+  setYears();
+}, 170);
