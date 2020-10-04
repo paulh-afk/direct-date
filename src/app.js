14f9e340bd8948ef7163003072df255c30756876
PauL <paul.helleu@orange.fr>
2020-10-04 19:24:39 +0200

    final version

--- src/app.js
+++ src/app.js
@@ -4,47 +4,58 @@ const dateDayElement = document.querySelector("h4");
 const dateHoursElement = document.querySelector(".content");
 const dateYearsElement = document.querySelector("footer");
 
-const dateDayNow = new Date().toLocaleString("fr-FR", {
-  day: "numeric",
-  weekday: "long",
-});
+const convertDate = (date) => {
+  const dateArr = (date) => {
+    const arr = date.split(" ");
+    firstLetterUp(arr);
+  };
 
-const dateYears = new Date().toLocaleString("fr-FR", {
-  year: "numeric",
-});
+  const firstLetterUp = (arr) => {
+    const text = arr.map((elem) => {
+      return elem.charAt(0).toUpperCase() + elem.substring(1);
+    });
+    dateDayElement.innerHTML = "";
+    dateDayElement.append(text.join(" - "));
+  };
 
-const DayNow = dateDayNow.toString();
-
-const dateArr = (date) => {
-  const arr = date.split(" ");
-  firstLetterUp(arr);
+  dateArr(date);
 };
 
-const firstLetterUp = (arr) => {
-  const text = arr.map((elem) => {
-    return elem.charAt(0).toUpperCase() + elem.substring(1);
-  });
-  dateDayElement.innerHTML = "";
-  dateDayElement.append(text.join(" - "));
+const setDate = () => {
+  const setHours = () => {
+    const dateHoursNow = new Date().toLocaleString("fr-FR", {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    });
+    dateHoursElement.innerHTML = "";
+    dateHoursElement.append(dateHoursNow);
+  };
+
+  const setDay = () => {
+    const dateDayNow = new Date().toLocaleString("fr-FR", {
+      day: "numeric",
+      weekday: "long",
+    });
+    convertDate(dateDayNow.toString());
+  };
+
+  const setYears = () => {
+    const dateYears = new Date().toLocaleString("fr-FR", {
+      year: "numeric",
+    });
+    dateYearsElement.innerHTML = "";
+    dateYearsElement.append(dateYears);
+  };
+
+  setInterval(() => {
+    setHours();
+  }, 170);
+
+  setInterval(() => {
+    setDay();
+    setYears();
+  }, 250);
 };
 
-const setHours = () => {
-  const dateHoursNow = new Date().toLocaleString("fr-FR", {
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-  });
-  dateHoursElement.innerHTML = "";
-  dateHoursElement.append(dateHoursNow);
-};
-
-const setYears = () => {
-  dateYearsElement.innerHTML = "";
-  dateYearsElement.append(dateYears);
-};
-
-setInterval(() => {
-  dateArr(DayNow);
-  setHours();
-  setYears();
-}, 170);
+setDate();
